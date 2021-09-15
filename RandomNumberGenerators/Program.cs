d39d0867e313895786b90aa4facaceeae92a1d6b
Opilane TTHK <opilane@tthk.ee>
2021-09-15 09:50:09 +0300

    random number

---
+++ RandomNumberGenerators/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace RandomNumberGenerators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int myRandomNumber = rnd.Next(1, 11);//number 1 kuni 10
+
+            Console.WriteLine($"Sinu juhuslik number on {myRandomNumber}");
+
+        }
+    }
+}
